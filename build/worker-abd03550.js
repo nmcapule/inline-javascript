31ed68070562fcb423c0aea0f31d57334d0c7ddf
nmcapule <llss.s.sls.lll@gmail.com>
2021-04-05 13:46:02 +0000

    Deploying to gh-pages from  @ 5b992ded16fb5503ab86482ce43c1ba21d461be7 🚀

--- build/worker-abd03550.js
+++
@@ -1,2 +0,0 @@
-import{e}from"./comlink-0c844d59.js";class t{async execute(e,t){self.context=t;const c=self.Function(`return (async () => {${e}})();`);return await c()}}e(t);export{t as API};
-//# sourceMappingURL=worker-abd03550.js.map
---
+++ sw.js
@@ -0,0 +1,56 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "c86eb9421c2f04a0e91ff6d6d228d796"
+  },
+  {
+    "url": "build/index.esm.js",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "build/p-000b7b07.js"
+  },
+  {
+    "url": "build/p-08dff46f.js"
+  },
+  {
+    "url": "build/p-6f73d9a6.entry.js"
+  },
+  {
+    "url": "build/p-a6d94535.js"
+  },
+  {
+    "url": "build/p-bb3fd708.js"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "0c129721ede7cd25304ebdd238d774ad"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
